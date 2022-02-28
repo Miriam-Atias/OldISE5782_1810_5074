6e4ea56ec36d083dfd8b69934085a8f4354772bd
Miriam Atias <73193927+MiriamAti@users.noreply.github.com>
2022-02-28 17:06:51 +0200

    exe1

---
+++ src/primitives/Vector.java
@@ -0,0 +1,80 @@
+package primitives;
+
+public class Vector extends Point {
+    public Vector(double x, double y, double z) {
+//        super(x,y,z);
+//        if(_xyz.equals(Double3.ZERO)){
+//            throw new IllegalArgumentException("Vector(0,0,0) is not allowed");
+//        }
+        this(new Double3(x, y, z));
+    }
+
+    /**
+     * primary  constructor for Vector class
+     *
+     * @param xyz head of vector starting from origin Point(0.0.0)
+     */
+    public Vector(Double3 xyz) {
+        super(xyz);
+        if (_xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException("Vector(0,0,0) is not allowed");
+        }
+    }
+
+    /**
+     * @return
+     */
+    public double lengthSquared() {
+        return _xyz._d1 * _xyz._d1
+                + _xyz._d2 * _xyz._d2
+                + _xyz._d3 * _xyz._d3;
+    }
+
+    /**
+     * @return
+     */
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    /**
+     * dot product between two vectors (scalar product)
+     *
+     * @param other the right vector of U.V
+     * @return scalar value of the dot product
+     * @link https://www.mathsisfun.com/algebra/vectors-dot-product.html
+     */
+    public double dotProduct(Vector other) {
+        return _xyz._d1 * other._xyz._d1
+                + _xyz._d2 * other._xyz._d2
+                + _xyz._d3 * other._xyz._d3;
+    }
+
+    /**
+     * cross product between two vectors (vectorial product)
+     * @param other other the right vector of U.V
+     * @return the vector resulting from the cross product (Right-hand rule)
+     * @link https://www.mathsisfun.com/algebra/vectors-cross-product.html
+     */
+    public Vector crossProduct(Vector other) {
+        double ax = _xyz._d1;
+        double ay = _xyz._d2;
+        double az = _xyz._d3;
+
+        double bx = other._xyz._d1;
+        double by = other._xyz._d2;
+        double bz = other._xyz._d3;
+
+        double cx = ay * bz - az * by;
+        double cy = az * bx - ax * bz;
+        double cz = ax * by - ay * bx;
+
+
+        return new Vector(cx, cy, cz);
+    }
+
+    public Vector normalize() {
+        double len = length();
+        return new Vector(_xyz.reduce(len));
+    }
+}
